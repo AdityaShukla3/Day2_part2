db4201bacd891fdff9c3fb37db1da0b3c077e913
Aditya Shukla <adityashukla2036@gmail.com>
2026-01-19 15:17:40 +0530

    Added data of 5 students

--- day2_part2/src/main/java/com/example/day2_part2/controller/Home.java
+++ day2_part2/src/main/java/com/example/day2_part2/controller/Home.java
@@ -4,11 +4,24 @@ import com.example.day2_part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student =new StudentModel(1,"Aditya","adi@gmail.com");
-        return student;
+    public ArrayList<StudentModel> getStudent(){
+        StudentModel student1 =new StudentModel(1,"Aditya","adi@gmail.com");
+        StudentModel student2 =new StudentModel(2,"Moksh","m@gmail.com");
+        StudentModel student3 =new StudentModel(3,"Prakhar","p@gmail.com");
+        StudentModel student4 =new StudentModel(4,"RamLal","r@gmail.com");
+        StudentModel student5 =new StudentModel(5,"Nitin","n@gmail.com");
+        ArrayList<StudentModel> list=new ArrayList<>();
+        list.add(student1);
+        list.add(student2);
+        list.add(student3);
+        list.add(student4);
+        list.add(student5);
+        return list;
     }
+
 }
